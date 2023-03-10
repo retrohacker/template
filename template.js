2a159d8146cd3acdd6a970c39e78939aa50b7a87
William Blankenship <code@blankenship.io>
2023-03-10 14:09:52 -0700

    Fix bug where mount() was replaced by host and make methods chainable

--- template.js
+++ template.js
@@ -18,44 +18,57 @@ class Template {
     if (changed) {
       this.emit("change", this.state);
     }
+    return this;
   }
   removeChild(name) {
     if (this.children[name]) {
       this.children[name].destroy();
     }
+    return this;
   }
   addChild(name, child) {
     this.removeChild(name);
     this.children[name] = child;
+    return this;
+  }
+  addChildren(obj) {
+    for (let name in obj) {
+      this.addChild(name, obj[name]);
+    }
+    return this;
   }
   getChild(name) {
     return this.children[name];
   }
-  mount(mount) {
-    if (this.mount) {
-      this.mount.innerText = "";
+  mount(host) {
+    if (this.host) {
+      this.host.innerText = "";
     }
-    this.mount = mount;
-    this.mount.appendChild(this.fragment);
+    this.host = host;
+    this.host.appendChild(this.fragment);
+    return this;
   }
   unmount() {
-    if (this.mount) {
-      this.mount.innerText = "";
+    if (this.host) {
+      this.host.innerText = "";
     }
     for (const child in this.children) {
       this.children[child].unmount();
     }
+    return this;
   }
   on(event, handler) {
     this.eventHandlers[event] = this.eventHandlers[event] || [];
     this.eventHandlers[event].push(handler);
+    return this;
   }
   emit(event, ...args) {
     if (!this.eventHandlers[event]) {
-      return;
+      return this;
     }
     this.eventHandlers[event].forEach((handler) => {
       handler(...args);
     });
+    return this;
   }
 }
