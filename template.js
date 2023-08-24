93c2d5afa4f6fbf8a7dc7bb49b2de2448cdf3b38
William Blankenship <code@blankenship.io>
2023-08-24 12:04:36 -0700

    typescript

--- template.js
+++ template.js
@@ -1,75 +1,122 @@
+"use strict";
 class Template {
-  constructor(id) {
-    if (id) {
-      this.fragment = document.getElementById(id).content.cloneNode(true);
+    constructor(id) {
+        if (id) {
+            const element = document.getElementById(id);
+            if (!element) {
+                throw new Error(`#${id} does not exist`);
+            }
+            if (!(element instanceof HTMLTemplateElement)) {
+                throw new Error(`#${id} is not a Template`);
+            }
+            this.fragment = element.content.cloneNode(true);
+        }
+        else {
+            this.fragment = null;
+        }
+        this.host = null;
+        this.eventHandlers = {};
+        this.children = {};
+        this.state = {};
+        this.destroyed = false;
     }
-    this.eventHandlers = {};
-    this.children = {};
-    this.state = {};
-  }
-  setState(obj) {
-    let changed = false;
-    for (let key in obj) {
-      if (this.state[key] !== obj[key]) {
-        changed = true;
-        this.state[key] = obj[key];
-      }
+    _issafe() {
+        if (this.destroyed) {
+            throw new Error("Template has already been destroyed");
+        }
     }
-    if (changed) {
-      this.emit("change", this.state);
+    setState(obj) {
+        let changed = false;
+        for (let key in obj) {
+            if (this.state[key] !== obj[key]) {
+                const value = obj[key];
+                changed = true;
+                if (value == undefined) {
+                    delete this.state[key];
+                }
+                else {
+                    this.state[key] = value;
+                }
+            }
+        }
+        if (changed) {
+            this.emit("change", this.state);
+        }
+        return this;
     }
-    return this;
-  }
-  removeChild(name) {
-    if (this.children[name]) {
-      this.children[name].unmount();
+    removeChild(name) {
+        const child = this.children[name];
+        if (child == undefined) {
+            return this;
+        }
+        child.unmount();
+        delete this.children[name];
+        return this;
     }
-    delete this.children[name];
-    return this;
-  }
-  addChild(name, child) {
-    this.removeChild(name);
-    this.children[name] = child;
-    return this;
-  }
-  addChildren(obj) {
-    for (let name in obj) {
-      this.addChild(name, obj[name]);
+    addChild(name, child) {
+        this.removeChild(name);
+        this.children[name] = child;
+        return this;
     }
-    return this;
-  }
-  getChild(name) {
-    return this.children[name];
-  }
-  mount(host) {
-    if (this.host) {
-      this.host.innerText = "";
+    addChildren(obj) {
+        for (let name in obj) {
+            const child = obj[name];
+            if (child == undefined) {
+                continue;
+            }
+            this.addChild(name, child);
+        }
+        return this;
     }
-    this.host = host;
-    this.host.appendChild(this.fragment);
-    return this;
-  }
-  unmount() {
-    if (this.host) {
-      this.host.innerText = "";
+    getChild(name) {
+        return this.children[name];
     }
-    for (const child in this.children) {
-      this.children[child].unmount();
+    mount(host) {
+        if (this.host) {
+            throw new Error("Already mounted");
+        }
+        if (!this.fragment) {
+            throw new Error("No fragment to mount");
+        }
+        this.host = host;
+        this.host.appendChild(this.fragment);
+        return this;
     }
-    return this;
-  }
-  on(event, handler) {
-    this.eventHandlers[event] = this.eventHandlers[event] || [];
-    this.eventHandlers[event].push(handler);
-    return this;
-  }
-  emit(event, ...args) {
-    if (!this.eventHandlers[event]) {
-      return this;
+    unmount() {
+        for (const name in this.children) {
+            const child = this.children[name];
+            if (child == undefined) {
+                continue;
+            }
+            child.unmount();
+        }
+        if (this.host) {
+            this.host.innerText = "";
+        }
+        this.host = null;
+        this.eventHandlers = {};
+        this.children = {};
+        this.state = {};
+        this.destroyed = true;
+        return;
     }
-    this.eventHandlers[event].forEach((handler) => {
-      handler(...args);
-    });
-    return this;
-  }
+    on(event, handler) {
+        const handlers = this.eventHandlers[event] || [];
+        this.eventHandlers[event] = handlers;
+        handlers.push(handler);
+        return this;
+    }
+    emit(event, ...args) {
+        const handlers = this.eventHandlers[event];
+        if (handlers == undefined) {
+            return this;
+        }
+        handlers.forEach((handler) => {
+            handler(...args);
+        });
+        return this;
+    }
+}
+if (module != undefined) {
+    module.exports = Template;
 }
---
+++ template.ts
@@ -0,0 +1,130 @@
+type EventHandlers = { [name: string]: Function[] };
+type State = { [name: string]: string | number | boolean };
+type Children = { [name: string]: Template };
+
+class Template {
+  fragment: Node | null;
+  eventHandlers: EventHandlers;
+  children: Children;
+  state: { [name: string]: string | number | boolean };
+  host: HTMLElement | null;
+  destroyed: Boolean;
+  constructor(id: string) {
+    if (id) {
+      const element = document.getElementById(id);
+      if (!element) {
+        throw new Error(`#${id} does not exist`);
+      }
+      if (!(element instanceof HTMLTemplateElement)) {
+        throw new Error(`#${id} is not a Template`);
+      }
+      this.fragment = element.content.cloneNode(true);
+    } else {
+      this.fragment = null;
+    }
+    this.host = null;
+    this.eventHandlers = {};
+    this.children = {};
+    this.state = {};
+    this.destroyed = false;
+  }
+  _issafe() {
+    if (this.destroyed) {
+      throw new Error("Template has already been destroyed");
+    }
+  }
+  setState(obj: State) {
+    let changed = false;
+    for (let key in obj) {
+      if (this.state[key] !== obj[key]) {
+        const value = obj[key];
+        changed = true;
+        if (value == undefined) {
+          delete this.state[key];
+        } else {
+          this.state[key] = value;
+        }
+      }
+    }
+    if (changed) {
+      this.emit("change", this.state);
+    }
+    return this;
+  }
+  removeChild(name: string) {
+    const child = this.children[name];
+    if (child == undefined) {
+      return this;
+    }
+    child.unmount();
+    delete this.children[name];
+    return this;
+  }
+  addChild(name: string, child: Template): Template {
+    this.removeChild(name);
+    this.children[name] = child;
+    return this;
+  }
+  addChildren(obj: Children): Template {
+    for (let name in obj) {
+      const child = obj[name];
+      if (child == undefined) {
+        continue;
+      }
+      this.addChild(name, child);
+    }
+    return this;
+  }
+  getChild(name: string): Template | undefined {
+    return this.children[name];
+  }
+  mount(host: HTMLElement): Template {
+    if (this.host) {
+      throw new Error("Already mounted");
+    }
+    if (!this.fragment) {
+      throw new Error("No fragment to mount");
+    }
+    this.host = host;
+    this.host.appendChild(this.fragment);
+    return this;
+  }
+  unmount() {
+    for (const name in this.children) {
+      const child = this.children[name];
+      if (child == undefined) {
+        continue;
+      }
+      child.unmount();
+    }
+    if (this.host) {
+      this.host.innerText = "";
+    }
+    this.host = null;
+    this.eventHandlers = {};
+    this.children = {};
+    this.state = {};
+    this.destroyed = true;
+    return;
+  }
+  on(event: string, handler: Function): Template {
+    const handlers = this.eventHandlers[event] || [];
+    this.eventHandlers[event] = handlers;
+    handlers.push(handler);
+    return this;
+  }
+  emit(event: string, ...args: any[]): Template {
+    const handlers = this.eventHandlers[event];
+    if (handlers == undefined) {
+      return this;
+    }
+    handlers.forEach((handler) => {
+      handler(...args);
+    });
+    return this;
+  }
+}
+
+if (module != undefined) {
+  module.exports = Template;
+}
